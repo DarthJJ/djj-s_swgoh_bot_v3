e691026c2c994f25107eeb70084b47e69d7dcdfe
Nick Janssen <nickrjanssen91@gmail.com>
2022-08-09 15:13:11 +0200

    Add missing abilityDao

---
+++ src/database/daos/abilityDao.ts
@@ -0,0 +1,26 @@
+import { iTable } from "./iTable.js";
+import { Ability } from "../../models/ability.js";
+import { DatabaseError } from "../../exceptions/databaseError.js";
+import { Repository } from "typeorm";
+import { container } from "tsyringe";
+
+export class AbilityDao implements iTable<Ability>{
+  private readonly _database: Repository<Ability>
+
+  constructor(database: Repository<Ability>) {
+    this._database = database;
+  }
+  getById(id: string | number): Promise<Ability | null> {
+    throw new Error("Method not implemented.");
+  }
+  async save(object: Ability): Promise<void> {
+    try {
+      await this._database.save(object);
+    } catch (exception: unknown) {
+      throw new DatabaseError("Something went wrong saving the ability", exception);
+    }
+  }
+  delete(id: string | number): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
